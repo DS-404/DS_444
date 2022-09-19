f8dbbd89311e435376fde705d90153cd2298412b
DS-404 <110836946+DS-404@users.noreply.github.com>
2022-09-19 21:56:50 +0600

    Add files via upload
    Just for test🤣

---
+++ DS444.py
@@ -0,0 +1,14 @@
+name = 'Mohammad'
+
+age =input(" Enter your age🤣 : ")
+age =+1
+
+#age =6
+
+if age >16 and name=='Mohammad':
+    print('Now you are a votar')
+elif age >10:
+	print('you is a bolod')    
+else:
+    print('you are not adult')
+
